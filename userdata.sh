35a9b4cb5e993f5e91c13b23c27ed17c1de65520
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-08 23:07:06 +0530

    Creating Internet Gateway

---
+++ userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yum install ansible -y &>>/opt/userdata.log
+
+ansible-pull -i localhost, -U https://github.com/Abhimanyu9555657/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
