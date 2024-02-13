c3f6dad3f4b3992b221e79c26be9695f8de07c7b
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-02-13 19:56:53 +0530

    changes

--- main.tf
+++ main.tf
@@ -45,3 +45,4 @@ resource "aws_route53_record" "main" {
   ttl     = 30
   records = [aws_instance.main.private_ip]
 }
+
--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,7 @@
 
 yum install ansible -y &>>/opt/userdata.log
 
-ansible-pull -i localhost, -U https://github.com/Abhimanyu9555657/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Abhimanyu9555657/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+
+
+
